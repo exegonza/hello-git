a044b2cc6f55215158ecb1853c08c0986e3e0cda
exegonza <puntoexe_666@hotmail.com>
2023-06-06 10:50:28 -0300

    Commit para corregir lo del stash

--- login.py
+++ login.py
@@ -1,2 +1,9 @@
 print ("aca es donde estaria todo lo que importa del Login")
+<<<<<<< Updated upstream
 print ("renglo para probar conflicto de Merges - se modifica en main")
+=======
+print ("renglo para probar conflicto de Merges - se modifica en main")
+
+print ("login V2)
+
+>>>>>>> Stashed changes
