86eb68ed9bfc7854a48e30be84cba977716b71b1
exegonza <puntoexe_666@hotmail.com>
2023-06-07 09:30:57 -0300

    Se resuelven problemas del main

--- login.py
+++ login.py
@@ -1,9 +1,5 @@
 print ("aca es donde estaria todo lo que importa del Login")
-<<<<<<< Updated upstream
-print ("renglo para probar conflicto de Merges - se modifica en main")
-=======
 print ("renglo para probar conflicto de Merges - se modifica en main")
 
-print ("login V2)
+print ("login V2")
 
->>>>>>> Stashed changes
