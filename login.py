bb629c76463a369d430d6f794c066e2d90c97cb5
exegonza <puntoexe_666@hotmail.com>
2023-06-07 09:37:00 -0300

    Commit para terminar Login

--- login.py
+++ login.py
@@ -1,2 +1,4 @@
 print ("aca es donde estaria todo lo que importa del Login")
 print ("renglo para probar conflicto de Merges - se modifica en main")
+
+print ("Esto es la version que quiero llevar a Main")
