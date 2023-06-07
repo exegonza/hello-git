ef0fcb4b23e6d8ddea3d64bb31038ad430c029c0
exegonza <puntoexe_666@hotmail.com>
2023-06-07 09:28:00 -0300

    Este commit es para dejar en orden

--- login.py
+++ login.py
@@ -1,6 +1,2 @@
 print ("aca es donde estaria todo lo que importa del Login")
-<<<<<<< HEAD
-print ("renglo para probar conflicto de Merges")
-=======
 print ("renglo para probar conflicto de Merges - se modifica en main")
->>>>>>> main
